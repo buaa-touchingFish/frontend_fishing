73dcf889b2e061c246ee6ab30a29e7e3209b9af4
Nat2Y <769468283@qq.com>
2023-11-03 14:03:04 +0800

    feat : 补充axios实例化封装

---
+++ src/api/axios.ts
@@ -0,0 +1,35 @@
+import router from '@/router';
+import { useAxiosStore } from '@/store/axiosStore';
+import { MessageApiInjection } from 'naive-ui/es/message/src/MessageProvider'
+const axiosStore = useAxiosStore();
+
+let request = axiosStore.axiosInstance
+
+export async function post(message: MessageApiInjection, url: string, props: any) {
+    try {
+        const res = await request.post(url, props);
+        if (res.status === 200) {
+            if (res.data.code === 200) {
+                return res.data.data;
+            } else {
+                let code = res.data.code
+                if (code === 400) {
+                    message.warning(res.data.message)
+                } else if (code === 401) {
+                    message.warning(res.data.message)
+                    router.push('/login')
+                }
+                return false;
+            }
+        } else {
+            message.error("网络错误！");
+            return false;
+        }
+    }
+    catch {
+        console.log('请求错误：' + url);
+        console.log(props);
+    }
+}
+
+export default request
---
+++ src/store/axiosStore.ts
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const useAxiosStore = defineStore('axiosStore', {
+    state: () => ({
+        persist: true,
+        axiosInstance: axios.create({
+            // TODO:补充后端接口
+            baseURL: '',
+            timeout: 5000,
+            headers: {
+                'Authorization': localStorage.getItem('token') || '',
+            },
+        }),
+    }),
+    actions: {
+        // 在获取token后给本axios实例补token
+        updateAuthorizationHeader(newToken: string) {
+            this.axiosInstance.defaults.headers['Authorization'] = newToken;
+        },
+    },
+});
