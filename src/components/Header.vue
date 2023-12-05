8da4255328607255903cfb0f55edf9d4d06cea4c
beanonlooker <3509769208@qq.com>
2023-12-05 21:38:07 +0800

    feat:使用封装的高级搜索

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,24 +32,7 @@
                         <div class="advancedSearchButton">高级搜索</div>
                     </template>
                     <div class="advancedSearchCard">
-                        <n-form ref="formRef" :model="searchCardModel" rules="" label-placement="left"
-                            label-width="80">
-                            <n-form-item label="作者" path="author">
-                                <n-input v-model:value="searchCardModel.author" placeholder="" />
-                            </n-form-item>
-                            <n-form-item label="机构" path="institution">
-                                <n-input v-model:value="searchCardModel.institution" placeholder="" />
-                            </n-form-item>
-                            <n-form-item label="出版物" path="publication">
-                                <n-popselect v-model:value="pubValue" :options="pubOptions" trigger="click">
-                                    <n-button>{{ pubValue }}</n-button>
-                                </n-popselect>
-                                <n-input v-model:value="searchCardModel.publication" placeholder="" />
-                            </n-form-item>
-                            <n-form-item label="日期范围" path="timestamp">
-                                <n-date-picker v-model:value="searchCardModel.timestamp" type="monthrange" clearable />
-                            </n-form-item>
-                        </n-form>
+                        <AdvancedSearch></AdvancedSearch>
                     </div>
                 </n-popover>
             </div>
@@ -66,7 +49,8 @@
 </template>
 
 <script setup lang='ts'>
-import { ref,Ref } from 'vue';
+import { ref } from 'vue';
+import AdvancedSearch from '@/components/search/AdvancedSearch.vue';
 import { Search12Filled, Person32Filled, Settings32Filled } from '@vicons/fluent';
 
 // 输入框
@@ -74,31 +58,7 @@ const searchValue = ref("")
 const searchOptions = ref([
 '@gmail.com', '@163.com', '@qq.com'
 ])
-const pubValue = ref<string>("会议")
-const pubOptions = [
-    {
-        label: '会议',
-        value: '会议'
-    },
-    {
-        label: '期刊',
-        value: '期刊'
-    },
-]
 
-interface SearchCardModelType {
-    author: string | null
-    institution: string | null
-    timestamp: Ref<[number, number]>
-    publication: string | null
-}
-
-const searchCardModel = ref<SearchCardModelType>({
-    author: null,
-    institution: null,
-    timestamp: ref<[number, number]>([946656000000, Date.now()]),
-    publication: null,
-})
 </script>
 
 <style scoped>
