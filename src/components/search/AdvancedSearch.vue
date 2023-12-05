c2635528bfb9a026d02ab9027a17bc48373da75d
Nat2Y <769468283@qq.com>
2023-12-05 21:33:28 +0800

    feat:封装高级搜索

---
+++ src/components/search/AdvancedSearch.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <n-form ref="formRef" :model="searchCardModel" rules="" label-placement="left" label-width="200">
+            <n-form-item label="作者" path="author">
+                <n-input v-model:value="searchCardModel.author" placeholder="" />
+            </n-form-item>
+            <n-form-item label="机构" path="institution">
+                <n-input v-model:value="searchCardModel.institution" placeholder="" />
+            </n-form-item>
+            <n-form-item label="出版物" path="publication">
+                <n-popselect v-model:value="pubValue" :options="pubOptions" trigger="click">
+                    <n-button>{{ pubValue }}</n-button>
+                </n-popselect>
+                <n-input v-model:value="searchCardModel.publication" placeholder="" />
+            </n-form-item>
+            <n-form-item label="日期范围" path="timestamp">
+                <n-date-picker v-model:value="searchCardModel.timestamp" type="monthrange" clearable />
+            </n-form-item>
+        </n-form>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { FormInst } from 'naive-ui';
+import { ref, Ref } from 'vue';
+
+//高级搜索卡片
+const formRef = ref<FormInst | null>(null)
+const pubValue = ref<string>("会议")
+const pubOptions = [
+    {
+        label: '会议',
+        value: '会议'
+    },
+    {
+        label: '期刊',
+        value: '期刊'
+    },
+]
+
+interface SearchCardModelType {
+    author: string | null
+    institution: string | null
+    timestamp: Ref<[number, number]>
+    publication: string | null
+}
+
+const searchCardModel = ref<SearchCardModelType>({
+    author: null,
+    institution: null,
+    timestamp: ref<[number, number]>([946656000000, Date.now()]),
+    publication: null,
+})
+</script>
+
+<style scoped></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -21,24 +21,7 @@
                     </div>
                     <div class="searchCardDiv" v-show="showCard">
                         <n-card hoverable>
-                            <n-form ref="formRef" :model="searchCardModel" rules="" label-placement="left"
-                                label-width="200">
-                                <n-form-item label="作者" path="author">
-                                    <n-input v-model:value="searchCardModel.author" placeholder="" />
-                                </n-form-item>
-                                <n-form-item label="机构" path="institution">
-                                    <n-input v-model:value="searchCardModel.institution" placeholder="" />
-                                </n-form-item>
-                                <n-form-item label="出版物" path="publication">
-                                    <n-popselect v-model:value="pubValue" :options="pubOptions" trigger="click">
-                                        <n-button>{{ pubValue }}</n-button>
-                                    </n-popselect>
-                                    <n-input v-model:value="searchCardModel.publication" placeholder="" />
-                                </n-form-item>
-                                <n-form-item label="日期范围" path="timestamp">
-                                    <n-date-picker v-model:value="searchCardModel.timestamp" type="monthrange" clearable />
-                                </n-form-item>
-                            </n-form>
+                            <AdvancedSearch></AdvancedSearch>
                         </n-card>
                     </div>
                 </div>
@@ -66,7 +49,7 @@ import Menu from '@/components/Menu.vue'
 import StarBackground from '@/components/Login/StarBackground.vue';
 import Clock from '@/components/Clock.vue';
 import { useRoute } from 'vue-router'
-import { FormInst } from 'naive-ui';
+import AdvancedSearch from '@/components/search/AdvancedSearch.vue';
 
 const route = useRoute()
 
@@ -87,37 +70,11 @@ const searchOptions = computed(() => {
     return result;
 });
 
-//高级搜索卡片
-const formRef = ref<FormInst | null>(null)
+// Card
 const showCard = ref(false)
 const changeShowCard = () => {
     showCard.value = !showCard.value
 }
-const pubValue = ref<string>("会议")
-const pubOptions = [
-    {
-        label: '会议',
-        value: '会议'
-    },
-    {
-        label: '期刊',
-        value: '期刊'
-    },
-]
-
-interface SearchCardModelType {
-    author: string | null
-    institution: string | null
-    timestamp: Ref<[number, number]>
-    publication: string | null
-}
-
-const searchCardModel = ref<SearchCardModelType>({
-    author: null,
-    institution: null,
-    timestamp: ref<[number, number]>([946656000000, Date.now()]),
-    publication: null,
-})
 </script>
 
 <style scoped>
