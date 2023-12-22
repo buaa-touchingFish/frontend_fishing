615a8b230119cf865d59a765687bb6602a872f97
Zhao Junlang <2739468442@qq.com>
2023-12-22 15:15:53 +0800

    refactor: collect part -- move some data and methods to pinia

--- src/components/userHome/ArticleItem.vue
+++ src/components/userHome/ArticleItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="root" :class="{ selected: selected }" @click="$emit('selectItem', title)">
+  <div class="item-container" :class="{ 'selected-style': selected }" @click="handleContainerClicked">
     <div class="check-box">
-      <n-checkbox v-model:checked="check"></n-checkbox>
+      <n-checkbox v-model:checked="check" @update:checked="handleCheckedChange"></n-checkbox>
     </div>
     <div class="right-container">
       <div class="title">{{ title }}</div>
@@ -17,31 +17,61 @@
   </div>
 </template>
 <script setup lang="ts">
-import { Ref, ref } from "vue";
+import { useCollectStore } from '@/store/collectStore'
+import { ref, computed } from "vue";
 import { NCheckbox, NTag } from "naive-ui";
-defineProps({
-  selected: Boolean,
-  userid: Number,
+
+const props = defineProps({
+  self_tag_idx: Number,
+  self_paper_idx: Number,
+  paper_id: String,
   title: String,
   author: String,
   journal: String,
   citations: Number,
   tags: Array as () => string[],
 });
-defineEmits(["change", "selectItem"]);
-let check: Ref<boolean> = ref(false);
+
+const collectStore = useCollectStore();
+
+// console.log('self idx', props.self_tag_idx, props.self_paper_idx);
+// console.log('active idx', props.active_tag_idx, props.active_paper_idx);
+
+const selected = computed(() => {
+  return (
+    props.self_tag_idx === collectStore.active_tag_idx &&
+    props.self_paper_idx === collectStore.active_paper_idx
+  );
+});
+
+const handleContainerClicked = () => {
+  // console.log("container clicked", props.self_tag_idx, props.self_paper_idx);
+  collectStore.set_active_idx(props.self_tag_idx, props.self_paper_idx);
+  console.log("active idx", collectStore.active_tag_idx, collectStore.active_paper_idx);
+  console.log("self idx", props.self_tag_idx, props.self_paper_idx);
+  console.log("selected", selected.value);
+};
+
+const check = ref(false);
+
+const handleCheckedChange = (value: boolean) => {
+  check.value = value;
+  console.log("check", check.value);
+  collectStore.change_item_checked(props.self_tag_idx, props.self_paper_idx, value);
+};
 </script>
 <style scoped>
-.selected {
-  background-color: #edf9ff;
-  cursor: unset;
-}
-
-.root {
+.item-container {
   width: 375px;
   height: 100px;
   display: flex;
   cursor: pointer;
+  /* border: 1px solid black; */
+  border-radius: 5px;
+  background-color: #ffffff;
+  box-shadow: #999999;
+  margin: 10px;
+  transition: filter 0.3s ease;
 
   .check-box {
     margin: 10px;
@@ -54,7 +84,7 @@ let check: Ref<boolean> = ref(false);
     flex-direction: column;
     justify-content: space-between;
     padding: 10px;
-    border-bottom: 1px solid #eaeaea;
+    /* border-bottom: 1px solid #eaeaea; */
 
     .title {
       font-size: 16px;
@@ -96,5 +126,11 @@ let check: Ref<boolean> = ref(false);
       }
     }
   }
+}
+
+.selected-style {
+  filter: brightness(95%);
+  /* background-color: #c6dafb; */
+  cursor: unset;
 }
 </style>
--- src/pages/user/Collect.vue
+++ src/pages/user/Collect.vue
@@ -1,17 +1,16 @@
 <template>
-  <div>
+  <div class="collect-container">
     <n-tabs v-model:value="value" type="card" addable closable tab-style="min-width: 80px;" @close="handleClose"
       @add="handleAdd">
-      <n-tab-pane v-for="tag in tags" :key="tag.name" :closable="notAll(tag.name)" :name="tag.name"
+      <n-tab-pane v-for="(tag, tidx) in collectStore.tags" :key="tag.name" :closable="notAll(tag.name)" :name="tag.name"
         :tab="tag2String(tag)">
-        <ArticleItem v-for="paper in tag.papers" :key="paper.title" :userid="user_id" :title="paper.title"
-          :author="paper.authors[0]" :journal="paper.journal" :citations="paper.citations" :tags="paper.tags" />
+        <div v-for="(paper, pidx) in tag.papers" :key="paper.paper_id">
+          <ArticleItem :self_tag_idx="tidx" :self_paper_idx="pidx" :paper_id="paper.paper_id" :title="paper.title"
+            :author="paper.authors[0]" :journal="paper.journal" :citations="paper.citations" :tags="paper.tags" />
+        </div>
       </n-tab-pane>
     </n-tabs>
     <n-modal v-model:show="showModal" preset="dialog" title="新标签">
-      <template #header>
-        <div>新标签</div>
-      </template>
       <div :class="{ label_input: statusIsError }" :style="{ '--pseudo-content': `'${pseudoContent}'` }">
         <n-input v-modal:value="newCreatingLabel" @input="updateCreatingStatus" :status="creatingStatus"
           placeholder="请输入标签名" />
@@ -29,84 +28,17 @@
 <script setup lang="ts">
 import api from '@/api/axios.ts';
 import ArticleItem from "@/components/userHome/ArticleItem.vue";
-import { Ref, ref, computed, onMounted } from "vue";
+import { useCollectStore, Tag } from '@/store/collectStore'
+import { ref, computed, onMounted } from "vue";
 import { NTabs, NTabPane, NModal, NInput, NButton, useMessage } from "naive-ui";
 
-class Tag {
-  public name: string;
-  public number: number;
-  public papers: Paper[];
-  constructor(name: string, number: number = 0, papers: Paper[] = []) {
-    this.name = name;
-    this.number = number;
-    this.papers = papers;
-  }
-  addPaper(paper: Paper) {
-    this.papers.push(paper);
-    this.number++;
-  }
-}
-
-class Paper {
-  public paper_id: string;
-  public title: string;
-  public authors: string[];
-  public journal: string;
-  public citations: number;
-  public tags: string[];
-  constructor(paper_id: string, title: string, authors: string[], journal: string, citations: number, tags: string[]) {
-    this.paper_id = paper_id;
-    this.title = title;
-    this.authors = authors;
-    this.journal = journal;
-    this.citations = citations;
-    this.tags = tags;
-  }
-}
-
-interface ArticleDetail {
-  aid: number;
-  title: string;
-  authors: string[];
-  abstract: string;
-  journal: string;
-  keywords: string[];
-  tags: string[];
-  citations: number;
-}
+const collectStore = useCollectStore();
 
 const message = useMessage();
 
-const user_id = ref(1);
-const getAllCollects = async (user_id: number) => {
-  const res = await api.get('/collect', {
-    params: {
-      user_id
-    }
-  })
-  const datas = res.data;
-  const labelMap = new Map<string, Tag>();
-  // 此时按照文章遍历, 将文章加入到指定的标签数据结构中
-  const tagAll = new Tag('全部');
-  for (const data of datas) {
-    const { paper_id, title, authors, publisher, cited_by_count, labels } = data;
-    const paper = new Paper(paper_id, title, authors, publisher, cited_by_count, labels);
-    for (const label of labels) {
-      if (!labelMap.has(label)) {
-        labelMap.set(label, new Tag(label));
-      }
-      const tag = labelMap.get(label);
-      tag?.addPaper(paper);
-      tagAll.addPaper(paper);
-    }
-  }
-  return [tagAll, ...labelMap.values()];
-}
-
 onMounted(() => {
-  getAllCollects(user_id.value).then((res) => {
-    tags.value = res;
-  })
+  // collectStore.getAllCollects();
+  collectStore.fakeData();
 })
 
 const tag2String = computed(() => {
@@ -122,33 +54,19 @@ const notAll = computed(() => {
 })
 
 const value = ref("全部");
-const tmpPapers = [
-  new Paper("1", "title1", ["author1"], "journal1", 1, ["tag1"]),
-  new Paper("2", "title2", ["author2"], "journal2", 2, ["tag2"]),
-  new Paper("3", "title3", ["author3"], "journal3", 3, ["tag3"]),
-  new Paper("4", "title4", ["author4"], "journal4", 4, ["tag4"]),
-  new Paper("5", "title5", ["author5"], "journal5", 5, ["tag5"]),
-];
-const tags: Ref<Tag[]> = ref([
-  new Tag("全部", 5, tmpPapers),
-  new Tag("医学", 2, tmpPapers),
-  new Tag("电气", 3, tmpPapers),
-  new Tag("AI", 1, tmpPapers),
-  new Tag("CS", 0, tmpPapers),
-]);
 
 const tag_string_array = computed(() => {
   const strs = [];
-  for (const tag of tags.value) {
+  for (const tag of collectStore.tags) {
     strs.push(tag.name);
   }
   return strs;
 })
 
-let hasClickedCreateBtn = ref(false);
-let newCreatingLabel = ref('');
+const hasClickedCreateBtn = ref(false);
+const newCreatingLabel = ref('');
 
-let creatingStatus = ref<string>();
+const creatingStatus = ref<string>();
 const updateCreatingStatus = (value: string) => {
   // console.log(`newCreatingLabel.value=${value}`)
   newCreatingLabel.value = value;
@@ -171,7 +89,7 @@ const handleAdd = () => {
 const requestCreateNewLabel = async (label_name: string) => {
   const res = await api.post('/collect/label', {
     params: {
-      user_id,
+      user_id: collectStore.user_id,
       paper_id: '',
       label_name
     }
@@ -197,10 +115,11 @@ const createLabel = () => {
     return;
   }
   // 可以创建
+  hasClickedCreateBtn.value = false;
   showModal.value = false;
   requestCreateNewLabel(newCreatingLabel.value).then((res) => {
     if (res === true) {
-      tags.value.push(new Tag(newCreatingLabel.value));
+      collectStore.push_new_tag(new Tag(newCreatingLabel.value));
       message.success(`成功创建标签: ${newCreatingLabel.value}`);
     } else {
       message.error('创建标签失败');
@@ -210,11 +129,18 @@ const createLabel = () => {
   });
 }
 
+// TODO
 const handleClose = (name: number) => {
   console.log(name);
 }
+
 </script>
 <style scoped>
+.collect-container {
+  margin-top: 15px;
+  width: 40vw;
+}
+
 .label_input::before {
   content: var(--pseudo-content, '');
   /* 默认值为空字符串 */
