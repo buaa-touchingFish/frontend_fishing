0c0882d7f1633366ba60494a88c55ac1f50c1d7a
beanonlooker <3509769208@qq.com>
2023-12-23 22:17:58 +0800

    feat:调试推荐文献接口完成

--- src/components/search/RecommendCard.vue
+++ src/components/search/RecommendCard.vue
@@ -1,28 +1,32 @@
 <template>
     <div class="recommendCardContainer shadow">
         <div class="recommendCardTop">文献推荐</div>
-        <div class="recommend" v-for="(recommend,index) in recommendList" :key="index">
-            <div class="title">{{ recommend.title }}</div>
-            <div class="QueryVolume">
-                <n-icon :size="20" :component="BookSearch24Regular"></n-icon>
-                &nbsp;搜索量：{{ recommend.QueryVolume }}
+        <div class="recommend" v-for="recommend in recommendList" :key="recommend.id">
+            <div class="titleContainer" :data-title="recommend.title">
+                <div class="title">{{ recommend.title }}</div>
+            </div>
+            <div class="clickVolume">
+                <n-icon :size="20" :component="CursorClick20Filled"></n-icon>
+                &nbsp;点击量：{{ recommend.browse }}
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang='ts'>
-import { ref } from 'vue';
-import { BookSearch24Regular } from '@vicons/fluent'
+import { ref,Ref,onMounted } from 'vue';
+import { CursorClick20Filled } from '@vicons/fluent'
+import { recommendPaper } from '@/models/model';
+import { get } from '@/api/axios';
+import { useMessage } from 'naive-ui';
 
-const props = defineProps(['recommend'])
+const message = useMessage();
 
-type recommend = {
-    title:string,
-    QueryVolume:number
-}
-const recommendList:recommend[] = props.recommend
-
+const recommendList:Ref<recommendPaper[]> = ref([]);
+onMounted(async () => {
+    recommendList.value = await get(message,'/paper/hot',{})
+    console.log(recommendList);
+})
 </script>
 
 <style scoped>
@@ -36,7 +40,6 @@ const recommendList:recommend[] = props.recommend
     box-sizing: border-box;
     border-radius: 10px;
     background-color: white;
-    cursor: pointer;
 }
 .recommendCardTop{
     width: 100%;
@@ -59,11 +62,42 @@ const recommendList:recommend[] = props.recommend
 .title{
     font-size: 16px;
     margin: 10px 0;
+    cursor: pointer;
     text-wrap: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    
+    &:hover{
+        text-decoration: underline;
+        color: var(--primary-100);
+    }
 }
-.QueryVolume{
+.titleContainer[data-title] {
+    position: relative;
+
+    &:hover:after { 
+        opacity: 1;
+        transition: all 0.1s ease 0.5s;
+        visibility: visible;
+    }
+    &:after {
+        content: attr(data-title);
+        background-color: var(--bg-200);
+        height: fit-content;
+        font-size: 12px;
+        position: absolute;
+        padding: 4px 10px;
+        right: 20px;
+        top: -1.5rem;
+        border-radius: 4px;
+        white-space: nowrap;
+        box-shadow: 0 0 4px rgba(0, 0, 0, 0.16);
+        z-index: 99999;
+        visibility: hidden;
+        opacity: 0;
+    }
+}
+.clickVolume{
     color:#999;
     display: flex;
     align-items: center;
